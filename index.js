e3f237135bb665a5afba1bbb7edee917a2bd1703
Jo Knightstep <jocoleknight@gmail.com>
2019-07-08 20:05:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const book1 = {
+    "id": 1,
+    "name": "The Wonderful Wizard of Oz",
+    "author": "L. Frank Baum"
+    "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Wizard_title_page.jpg/220px-Wizard_title_page.jpghttp://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
+    "price": 19.00,
+    "sellingPoints": [
+        "America's greatest and best-loved homegrown fairytale",
+        "Translated in over 50 languages.",
+        "One of the most-read children's books."
+    ]
+}
+
+const book2 = {
+    "id": 2,
+    "name": "Alice's Adventure in Wonderland",
+    "author": "Louis Carrol"
+    "pictureUrl": "https://https://cannonballread3.files.wordpress.com/2012/01/alice06.jpg.wikimedia.org/wikipedia/commons/thumb/d/d2/Wizard_title_page.jpg/220px-Wizard_title_page.jpghttp://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
+    "price": 18.65,
+    "sellingPoints": [
+        "tale plays with logic, giving the story lasting popularity with adults as well as with children",
+        "one of the best examples of the literary nonsense genre",
+        "I am the Carpenter. Goo goo g'joob"
+    ]
+}
